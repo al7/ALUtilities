3286bd4111b9cd8c911295a2ab7dc4c7bfb664b7
Alex Leite <admin@al7dev.com>
2014-03-24 11:09:30 -0500

    Initial Commit

---
+++ ALUtilities/ALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALAppDelegate.h
+//  ALUtilities
+//
+//  Created by Alexandre Leite on 3/24/14.
+//  Copyright (c) 2014 al7dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
